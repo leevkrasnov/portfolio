ce1e6cd7002c57357c4a6efea60daa22044ab95c
Lev <leevkrasnov@gmail.com>
2024-10-30 20:20:47 +0500

    fix: спрятана кнопка смены языка до ее реализации

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -17,12 +17,12 @@ const Header = () => {
 
       <div className="flex items-center justify-end space-x-3 md:space-x-6">
         <ThemeToggleSwitch />
-        <button aria-label="Change Language" className="nav-element">
+        {/* <button aria-label="Change Language" className="nav-element">
           <Globe
             strokeWidth={1.2}
             className="h-4 w-4 text-gray-800 dark:text-gray-400 md:h-6 md:w-6 lg:h-7 lg:w-7"
           />
-        </button>
+        </button> */}
       </div>
     </header>
   );
